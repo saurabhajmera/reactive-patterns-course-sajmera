c00d8e18b9ef4303ed3bf7c7eab036e718aa2610
Saurabh Ajmera <sajmera@netflix.com>
2018-01-15 12:59:48 -0800

    dom api to unsubscribe from browser events

---
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'browser-event-experiments',
+  templateUrl: './browser-event-experiments.component.html',
+  styleUrls: ['./browser-event-experiments.component.css']
+})
+export class BrowserEventExperimentsComponent implements OnInit {
+
+  hoverSection:HTMLElement;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+    this.hoverSection.addEventListener('mousemove',onMouseMove);
+  }
+
+  unsubscribe(){
+    console.log('Called unsubscribe()');
+    this.hoverSection.removeEventListener('mousemove',onMouseMove);
+  }
+
+}
+
+function onMouseMove(ev:MouseEvent) {
+  console.log(ev);
+
+}
